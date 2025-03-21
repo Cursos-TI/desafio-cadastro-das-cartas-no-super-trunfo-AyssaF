8b4ff2bcf40cec938c5b743b8836a76f925c339e
AyssaF <ayssaferraz2017@gmail.com>
2025-03-21 00:57:13 +0000

    Criei o printf

---
+++ xadrez.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    printf("Desafio Xadrez!\n");
+
+    int populacao;
+    float area;
+    float PIB; 
+    int turisticos;
+
+    printf("Digite o numero da população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite turisticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %f\n", PIB);
+    printf("Turisticos: %d\n", turisticos);
+
+    return 0;
+
+
+
+
+
+
+
+}
